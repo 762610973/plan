6913d7350c24e7648aa38aa8332489c0dddeead3
XiaoLiang <762610973@qq.com>
2023-07-24 19:58:10 +0800

    🔊 front: list; rust: No.1.1

--- Rust/rust1/src/main.rs
+++ Rust/rust1/src/main.rs
@@ -1,7 +1,20 @@
+// use std::prelude::*;
+// crate: 完整的编译单元，生成一个lib或者exe可执行文件
+// mod: crate内部，即namespace
+// println！ 宏 --> 更好地编译器检查
 fn main() {
     println!("Hello, world!");
     let  res = add(1, 2);
-    println!("res: {}",res)
+    println!("res: {}",res);
+    println!("{}",1);
+    println!("{:o}",9);
+    println!("{:x}",255);
+    println!("{:X}",255);
+    println!("{:p}",&0);
+    println!("二进制：{:b}",8);
+    println!("{:?}","debug");
+    println!("{:#?}",("带有缩进","带有换行的debug"));
+    println!("{a} {b}",a=1,b=2);
 
 }
 
