7ed661239a68785a40834ddab41b9bae017a12a6
XiaoLiang <762610973@qq.com>
2023-07-26 10:03:21 +0800

    📝: rust: basic_type

---
+++ Rust/rust1-variable-type/src/basic_type.rs
@@ -0,0 +1,87 @@
+// !bool
+fn test_type1() {
+	let x = true;
+	let y: bool = !x;
+
+	let z = x && y;
+	let z = x & y;
+	let z = x ^ y;
+	fn logical_op(x: i32, y: i32) {
+		let z: bool = x < y;
+		if x >= y {} else {}
+	}
+}
+
+// !char
+// 描述任意一个unicode字符,占用四个字节
+fn test_char1() {
+	let love = '&';
+	let c1 = '\n';
+	let c2 = '\x7f';
+}
+
+// 使用一个字母b在字符或者字符串前面,代表这个字面量存储在u8类型数组中
+fn test_char_u8() {
+	let x: u8 = 1;
+	let y: u8 = b'a';
+	let s: &[u8; 5] = b"hello";
+}
+
+fn test_number() {
+	let v1: i32 = 32;// 十进制
+	let v2: i32 = 0xFF; // 十六进制
+	let v3: i32 = 0o44; //以0o开头代表八进制表示
+	let v4: i32 = 0b1001; // 以0b开头代表二进制表示
+	let v5 = 0x_1234_abcd; // !可以使用下划线分割
+}
+
+
+fn test_number1() {
+	let x = 4_i32;  // 后置类型
+	let x = 400i32;  // 后置类型
+	// let x:i32  = 4;
+	println!("9 power 3 = {}", x.pow(3));
+}
+
+// 可能会溢出
+// rustc -C overflow-checks=no test.rs
+fn overflow(m: i8, n: i8) {
+	println!("{:?}", m + n);
+}
+
+fn test_overflow() {
+	let i = 100_i8;
+	println!("{:?}", i.checked_add(i));
+}
+
+fn test_float() {
+	let f1 = 123.0f64;
+	let f2 = 123.4f64;
+	let f3 = 123.4_f64;
+	let f4: f64 = 2.;
+	let f5 = 12E+99_f64; //科学计数法
+}
+
+fn test_point() {
+	//! Box<T> 指向类型T的,具有所有权的指着,有权释放内存
+	//! &T 指向类型T的借用指针,也称为引用,无权 释放内存,无权写数据
+	//! &mut T 指向类型T的mut型借用指针,无权释放内存,有权写内存
+	//! *const T 指向类型T的只读裸指针,没有生命周期信息,无权写数据
+	//! *mut T 只想类型T的可读写裸指针,没有生命周期信息,有权写数据
+
+
+	// Rc<T>  指向类型T的引用计数指针,共享所有权,线程不安全
+	// Arc<T> 只想类型T的原子型引用计数指针,共享所有权,线程安全
+	// Cow<'a,T> Clone-on-write, 写时复制指针,可能是借用指针,也可能是具有所有权的指针
+}
+
+fn type_change() {
+	let v1: i8 = 41;
+	//! 显示标记类型转换
+	let v2: i16 = v1 as i16;
+	println!("{:?}", v2);
+	let i = 42;
+	// 多次转换
+	let p = &i as *const i32 as *mut i32;
+	println!("{:p}", p);
+}
--- Rust/rust1-variable-type/src/main.rs
+++ Rust/rust1-variable-type/src/main.rs
@@ -1,87 +1,93 @@
+mod basic_type;
+
 use std::sync::atomic::AtomicBool;
 
 fn main() {
-    grow(1,1);
-    // 语法歧义少,语法分析更容易
-    // 方便引入类型推导功能
-    // 方便 模式解构
-    // let variable:i32 = 100;
-    // 将mut a1看做一个组合
-    // let mut a1 = 1;
-    // let (mut a2,mut a3) = (2,3);
-    //  不能使用未初始化的变量,编译器会做执行路径的静态分析
-    // let unused :i32;
-    // println!("{:?}", unused);
-    // used binding `unused` isn't initialized
+	grow(1, 1);
+	// 语法歧义少,语法分析更容易
+	// 方便引入类型推导功能
+	// 方便 模式解构
+	// let variable:i32 = 100;
+	// 将mut a1看做一个组合
+	// let mut a1 = 1;
+	// let (mut a2,mut a3) = (2,3);
+	//  不能使用未初始化的变量,编译器会做执行路径的静态分析
+	// let unused :i32;
+	// println!("{:?}", unused);
+	// used binding `unused` isn't initialized
 }
 
-fn test(condition:bool) {
-    let x:i32;
-    if condition {
-        x = 1;  // 初始化x不需要x是mut的,因为是初始化,不是修改
-        println!("{:?}", 1);
-    }
-    // 如果条件不满足,x没有被初始化
-    // 不使用x就可以
+fn test(condition: bool) {
+	let x: i32;
+	if condition {
+		x = 1;  // 初始化x不需要x是mut的,因为是初始化,不是修改
+		println!("{:?}", 1);
+	}
+	// 如果条件不满足,x没有被初始化
+	// 不使用x就可以
 }
 // 类型没有"默认构造函数",变量没有"默认值"
 
 fn shadow() {
-    let x = "hello, world";
-    println!("{:?}", x);
-    let x = 5;
-    println!("{:?}", x);
+	let x = "hello, world";
+	println!("{:?}", x);
+	let x = 5;
+	println!("{:?}", x);
 }
 
 fn shadow1() {
-    let v = Vec::new();
-    let mut v = v;
-    v.push(1);
-    println!("{:?}", v);
+	let v = Vec::new();
+	let mut v = v;
+	v.push(1);
+	println!("{:?}", v);
 }
 
 fn get_type() {
-    let elem = 5u8;
-    let mut vec = Vec::new();
-    vec.push(elem);
-    println!("{:?}",vec);
+	let elem = 5u8;
+	let mut vec = Vec::new();
+	vec.push(elem);
+	println!("{:?}", vec);
 }
 
 // 只允许局部变量/全局变量实现类型推导
 fn get_type2() {
-    let play = [
-        ("jack",20),("jane",23)
-    ];
-    let players:Vec<_> = play.iter()
-        .map(|&(player,_score)| {
-        player
-    })
-    .collect();
-    println!("{:?}",players);
+	let play = [
+		("jack", 20), ("jane", 23)
+	];
+	let players: Vec<_> = play.iter()
+		.map(|&(player, _score)| {
+			player
+		})
+		.collect();
+	println!("{:?}", players);
 }
+
 // 类型别名,Go中的类型别名: type Age = uint32
 type Age = u32;
-fn grow(age:Age,year: u32) -> Age {
-    if age == year {
-        println!("{}", "true");
-    } else {
-        println!("{:?}", "false");
 
-    }
-    return age+year;
+fn grow(age: Age, year: u32) -> Age {
+	if age == year {
+		println!("{}", "true");
+	} else {
+		println!("{:?}", "false");
+	}
+	return age + year;
 }
+
 // 静态变量
 // 全局变量的初始化必须是编译器可以确定的常量
 // 带有mut修饰的全局变量,在使用的时候必须使用unsafe关键字
 // Rust禁止在声明static变量的时候调用普通函数，或者利用语句块调用其他非const代码
 // 允许调用const fn
+// !全局变量只有一个实例,所有的引用都会指向一个相同的地址
 static GLOBAL: i32 = 0;
 
 fn global() {
-    println!("{:?}", GLOBAL);
-    use std::sync::atomic::AtomicBool;
-    static FLAG:AtomicBool = AtomicBool::new(true);
+	println!("{:?}", GLOBAL);
+	use std::sync::atomic::AtomicBool;
+	static FLAG: AtomicBool = AtomicBool::new(true);
 }
+
 // const声明常量,不具备类似语句的模式匹配功能
 // 可能会被内联优化
-const GLOBAL1:i32 = 0;
+const GLOBAL1: i32 = 0;
