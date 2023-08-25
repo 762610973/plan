4b6774585b2f2a9d86af478196a2a4334b0a5a78
XiaoLiang <762610973@qq.com>
2023-08-25 22:56:40 +0800

    ✨ Go: contextWithValue; JS: hello world

--- Go/source/context/context.go
+++ Go/source/context/context.go
@@ -1,7 +1,19 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 func main() {
-	_ = context.Background()
+	contextWithValue()
+}
+
+func contextWithValue() {
+	ctx := context.Background() // parent context
+	ctx1 := context.WithValue(ctx, "key1", "val1")
+	fmt.Println("get value from ctx1, this value was set by ctx1, value: ", ctx1.Value("key1"))
+	ctx2 := context.WithValue(ctx1, "key2", "val2")
+	fmt.Println("get value from ctx2, this value was set by ctx2, value: ", ctx2.Value("key2"))
+	fmt.Println("get value from ctx2, this value was set by ctx1, value: ", ctx2.Value("key1"))
 }
